465154d899c982d79949fececfd09b6090599444
ajfAfg <56056962+ajfAfg@users.noreply.github.com>
2021-01-11 21:33:21 +0900

    Change the domain into a flexible domain
    for the future.

--- apserver/src/ap_server.erl
+++ apserver/src/ap_server.erl
@@ -1,17 +1,17 @@
 -module(ap_server).
--export([start/0]).
+-export([start/1]).
 
-start() ->
+start([Domain | _]) ->
 	inets:start(),
-	{ok, Pid} = inets:start(httpd, prop_list() ),
+	{ok, Pid} = inets:start(httpd, prop_list(Domain) ),
 	Pid.
 
-prop_list() ->
+prop_list(Domain) ->
 	[{port, 8080},
 	 {server_name, "ap_server"},
 	 {server_root, "."},
 	 {document_root, "."},
-	 {bind_address, "localhost"},
+	 {bind_address, Domain},
 %	 {modules, [mod_test2]}].
 %	 {modules, [mod_alias, mod_auth, mod_esi, mod_actions, mod_cgi, mod_dir, mod_get, mod_head, mod_log, mod_disk_log, mod_test2]}].
 	 {modules, [mod_alias, mod_auth, mod_esi, mod_actions, mod_head, mod_log, mod_disk_log, mod_room]}].
--- webserver/web_server.erl
+++ webserver/web_server.erl
@@ -1,15 +1,15 @@
 -module(web_server).
--export([start/0]).
+-export([start/1]).
 
-start() ->
+start([Domain | _]) ->
 	inets:start(),
-	{ok, Pid} = inets:start(httpd, prop_list() ),
+	{ok, Pid} = inets:start(httpd, prop_list(Domain) ),
 	Pid.
 
-prop_list() ->
+prop_list(Domain) ->
 	[{port, 80},
 	 {server_name, "web_server"},
 	 {server_root, "."},
 	 {document_root, "."},
-	 {bind_address, "localhost"},
+	 {bind_address, Domain},
 	 {directory_index, ["index.html"]}].
