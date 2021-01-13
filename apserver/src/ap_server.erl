1f460eb0a9c452b3cef67819bdf31c43a72bd6d0
ajfAfg <56056962+ajfAfg@users.noreply.github.com>
2021-01-14 03:48:37 +0900

    Add the module mod_dealer

--- apserver/src/ap_server.erl
+++ apserver/src/ap_server.erl
@@ -1,18 +1,18 @@
 -module(ap_server).
 -export([start/1]).
 
-%start([Domain | _]) ->
-start(Domain) ->
+%start([Host | _]) ->
+start(Host) ->
 	inets:start(),
-	{ok, Pid} = inets:start(httpd, prop_list(Domain) ),
+	{ok, Pid} = inets:start(httpd, prop_list(Host) ),
 	Pid.
 
-prop_list(Domain) ->
+prop_list(Host) ->
 	[{port, 8080},
 	 {server_name, "ap_server"},
 	 {server_root, "."},
 	 {document_root, "."},
-	 {bind_address, Domain},
+	 {bind_address, Host},
 %	 {modules, [mod_test2]}].
 %	 {modules, [mod_alias, mod_auth, mod_esi, mod_actions, mod_cgi, mod_dir, mod_get, mod_head, mod_log, mod_disk_log, mod_test2]}].
-	 {modules, [mod_alias, mod_auth, mod_esi, mod_actions, mod_head, mod_log, mod_disk_log, mod_room]}].
+	 {modules, [mod_alias, mod_auth, mod_esi, mod_actions, mod_head, mod_log, mod_disk_log, mod_room, mod_dealer]}].
