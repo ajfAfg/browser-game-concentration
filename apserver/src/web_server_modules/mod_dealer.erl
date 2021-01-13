1c971a600b13828e722faa1145b55575e2c99472
ajfAfg <56056962+ajfAfg@users.noreply.github.com>
2021-01-14 03:43:24 +0900

    Change a position of pattern matching for an argument
    It seems like that the server modules have to give the other modules the argument if do not use

---
+++ apserver/src/web_server_modules/mod_dealer.erl
@@ -0,0 +1,23 @@
+-module(mod_dealer).
+-export([do/1]).
+-include_lib("inets/include/httpd.hrl").
+
+do(ModData) ->
+	case ModData#mod.request_uri =:= "/dealer" andalso ModData#mod.method =:= "POST" of
+		true ->
+			handle(ModData);
+		false ->
+			{proceed, ModData}
+	end.
+
+handle(ModData) ->
+	EntityList = uri_string:dissect_query(ModData#mod.entity_body),
+	UserId = misc:get_entity_with_tag(EntityList, "user_id"),
+	MathingId = misc:get_entity_with_tag(EntityList, "matching_id"),
+			   
+	Deck = misc:shuffle(deck:generate_deck() ),
+	CSV  = deck:convert_csv(Deck),
+	Head = [{code,200}, {content_type,"text/csv"}, {content_length,misc:len(CSV)}],
+	Body = [CSV],
+	NewData = [{response, {response,Head,Body}}],
+	{break, NewData}.
--- apserver/src/web_server_modules/mod_room.erl
+++ apserver/src/web_server_modules/mod_room.erl
@@ -2,10 +2,26 @@
 -export([do/1]).
 -include_lib("inets/include/httpd.hrl").
 
-do(ModData) when ModData#mod.request_uri =:= "/room" andalso ModData#mod.method =:= "POST" ->
+do(ModData) ->
+	case ModData#mod.request_uri =:= "/room" andalso ModData#mod.method =:= "POST" of
+		true ->
+			handle(ModData);
+		false ->
+			{proceed, ModData}
+	end.
+
+handle(ModData) ->
 	EntityList = uri_string:dissect_query(ModData#mod.entity_body),
-	UserId = misc:get_entity_with_tag(EntityList, "id"),
-	MatchingId = room_server:match(UserId),
+	UserId = misc:get_entity_with_tag(EntityList, "user_id"),
+%	MatchingId = room_server:wait_for_matching(UserId),
+	MatchingId = case room_server:wait_for_matching(UserId) of
+					 {matching, Id} ->
+						 Id;
+					 no_matching ->
+						 "no_matching"
+				 end,
+%	MatchingId = room_server:hello(),
+%	MatchingId = "987654321",
 	Head = [{code,200}, {content_type,"text/plain"}, {content_length,misc:len(MatchingId)}],
 	Body = [MatchingId],
 	NewData = [{response, {response,Head,Body}}],
@@ -15,4 +31,5 @@ do(ModData) when ModData#mod.request_uri =:= "/room" andalso ModData#mod.method 
 %	Head = [{code,200}, {content_type,"text/csv"}, {content_length,integer_to_list(length(CSV))}],
 %	Body = [CSV],
 %	NewData = [{response, {response,Head,Body}}],
-	{proceed, NewData}.
+%	{proceed, NewData}.
+	{break, NewData}.
