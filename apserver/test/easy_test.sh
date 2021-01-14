4fc732425700453f168ec5ed66a6a9bad202e0b3
ajfAfg <56056962+ajfAfg@users.noreply.github.com>
2021-01-15 01:49:26 +0900

    Create very very easy test for server programs
    This wiil be deleted.

---
+++ apserver/test/easy_test.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+curl -X POST -d 'user_id=1' localhost:8080/room &
+matching_id=`curl -X POST -d 'user_id=123456789' localhost:8080/room`
+
+sleep 1
+
+curl -X POST -d 'user_id=1' -d "matching_id=${matching_id}" localhost:8080/dealer
+
+sleep 1
+
+curl -X POST -d 'user_id=1' -d "matching_id=${matching_id}" localhost:8080/match &
+curl -X POST -d 'user_id=1' -d "matching_id=${matching_id}" -d 'x=1' -d 'y=2' localhost:8080/match
