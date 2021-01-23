e5fea815bc4adefb1d545478ca8ff775430d3df6
ajfAfg <56056962+ajfAfg@users.noreply.github.com>
2021-01-24 03:19:23 +0900

    Deal with new API

--- apserver/test/easy_test.sh
+++ apserver/test/easy_test.sh
@@ -10,5 +10,5 @@ curl -X POST -d 'user_id=2' -d "matching_id=${matching_id}" localhost:8080/deale
 
 sleep 1
 
-curl -X POST -d 'user_id=1' -d "matching_id=${matching_id}" localhost:8080/match &
-curl -X POST -d 'user_id=2' -d "matching_id=${matching_id}" -d 'x=1' -d 'y=2' localhost:8080/match
+curl -X POST -d 'user_id=1' -d "matching_id=${matching_id}" -d 'turn=1' localhost:8080/match &
+curl -X POST -d 'user_id=2' -d "matching_id=${matching_id}" -d 'turn=1' -d 'x=1' -d 'y=2' localhost:8080/match
