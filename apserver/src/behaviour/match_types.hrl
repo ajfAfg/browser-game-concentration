3659bc7185555635f12d4e094fe434635e04ef65
ajfAfg <56056962+ajfAfg@users.noreply.github.com>
2021-01-24 03:14:23 +0900

    Add types related move

--- apserver/src/behaviour/match_types.hrl
+++ apserver/src/behaviour/match_types.hrl
@@ -1,3 +1,9 @@
 -type matching_id() :: string().
 -type user_id() :: string().
 -type matching_info() :: string().
+
+-type x() :: string().
+-type y() :: string().
+-type move() :: {x(), y()}.
+
+-type turn() :: pos_integer().
