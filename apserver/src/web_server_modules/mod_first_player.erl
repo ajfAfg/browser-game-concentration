119b41c6ad50c9f25b9c5fd89d5a6b35ea1cf3fb
ajfAfg <56056962+ajfAfg@users.noreply.github.com>
2021-01-25 01:09:27 +0900

    Add the HTTP header Access-Control-Allow-Origin
    For accessing the Concentration web pages

--- apserver/src/web_server_modules/mod_first_player.erl
+++ apserver/src/web_server_modules/mod_first_player.erl
@@ -1,6 +1,7 @@
 -module(mod_first_player).
 -export([do/1]).
 -include_lib("inets/include/httpd.hrl").
+-include_lib("src/server_config.hrl").
 
 do(ModData) ->
 	case ModData#mod.request_uri =:= "/first_player" andalso ModData#mod.method =:= "POST" of
@@ -16,7 +17,7 @@ handle(ModData) ->
 	UserId = misc:get_entity_with_tag(EntityList, "user_id"),
 
 	Reply = atom_to_list(deciding_first_player_server:is_first_player(MathingId, UserId) ),
-	Head = [{code,200}, {content_type,"text/csv"}, {content_length,misc:len(Reply)}],
+	Head = [{code,200}, {content_type,"text/plain"}, {content_length,misc:len(Reply)}, ?ACAO],
 	Body = [Reply],
 	NewData = [{response, {response,Head,Body}}],
 	{break, NewData}.
