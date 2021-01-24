aed0629f78ad4bd4b5521547e95d19c3fe3f5da8
ajfAfg <56056962+ajfAfg@users.noreply.github.com>
2021-01-24 18:09:59 +0900

    Create a web module of dealing a first player

---
+++ apserver/src/web_server_modules/mod_first_player.erl
@@ -0,0 +1,22 @@
+-module(mod_first_player).
+-export([do/1]).
+-include_lib("inets/include/httpd.hrl").
+
+do(ModData) ->
+	case ModData#mod.request_uri =:= "/first_player" andalso ModData#mod.method =:= "POST" of
+		true ->
+			handle(ModData);
+		false ->
+			{proceed, ModData}
+	end.
+
+handle(ModData) ->
+	EntityList = uri_string:dissect_query(ModData#mod.entity_body),
+	MathingId = misc:get_entity_with_tag(EntityList, "matching_id"),
+	UserId = misc:get_entity_with_tag(EntityList, "user_id"),
+
+	Reply = atom_to_list(deciding_first_player_server:is_first_player(MathingId, UserId) ),
+	Head = [{code,200}, {content_type,"text/csv"}, {content_length,misc:len(Reply)}],
+	Body = [Reply],
+	NewData = [{response, {response,Head,Body}}],
+	{break, NewData}.
