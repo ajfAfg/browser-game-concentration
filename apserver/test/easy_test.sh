7520f3d557dce34301a20a8db43dca379a272868
ajfAfg <56056962+ajfAfg@users.noreply.github.com>
2021-01-24 18:12:46 +0900

    Add requests of judging whether a player is a first player

--- apserver/test/easy_test.sh
+++ apserver/test/easy_test.sh
@@ -10,6 +10,11 @@ curl -X POST -d 'user_id=2' -d "matching_id=${matching_id}" localhost:8080/deale
 
 sleep 1
 
+curl -X POST -d 'user_id=1' -d "matching_id=${matching_id}" localhost:8080/first_player &
+curl -X POST -d 'user_id=2' -d "matching_id=${matching_id}" localhost:8080/first_player &
+
+sleep 1
+
 
 curl -X POST -d 'user_id=2' -d "matching_id=${matching_id}" -d 'turn=1' -d 'x=1' -d 'y=2' localhost:8080/match &
 curl -X POST -d 'user_id=1' -d "matching_id=${matching_id}" -d 'turn=1' localhost:8080/match &
