adefe97fdbf856fa6d474e57346ae2d74232c246
ajfAfg <56056962+ajfAfg@users.noreply.github.com>
2021-01-26 17:10:41 +0900

    Change how to make
    It is simpler than before.

--- webserver/src/web_server.erl
+++ webserver/src/web_server.erl
@@ -1,17 +1,18 @@
 -module(web_server).
--export([start/1]).
+-export([start/0]).
 
-start(Host) ->
+-include_lib("src/server_config.hrl").
+
+start() ->
 	inets:start(),
-	{ok, Pid} = inets:start(httpd, prop_list(Host) ),
-	Pid.
+	inets:start(httpd, prop_list() ).
 
-prop_list(Host) ->
+prop_list() ->
 	[{port, 80},
 	 {server_name, "web_server"},
 	 {server_root, "."},
 	 {document_root, "."},
-	 {bind_address, Host},
+	 {bind_address, ?HOST},
 	 {directory_index, ["index.html"]},
 	 {mime_types, [
 				   {"html", "text/html"},
