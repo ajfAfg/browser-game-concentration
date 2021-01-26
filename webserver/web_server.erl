794d35526d2cabb6d9d001564e27cf1047aa32ee
ajfAfg <56056962+ajfAfg@users.noreply.github.com>
2021-01-26 14:55:14 +0900

    Rename an argument name
    refactoring

--- webserver/web_server.erl
+++ webserver/web_server.erl
@@ -1,17 +1,17 @@
 -module(web_server).
 -export([start/1]).
 
-start([Domain | _]) ->
+start([Host | _]) ->
 	inets:start(),
-	{ok, Pid} = inets:start(httpd, prop_list(Domain) ),
+	{ok, Pid} = inets:start(httpd, prop_list(Host) ),
 	Pid.
 
-prop_list(Domain) ->
+prop_list(Host) ->
 	[{port, 80},
 	 {server_name, "web_server"},
 	 {server_root, "."},
 	 {document_root, "."},
-	 {bind_address, Domain},
+	 {bind_address, Host},
 	 {directory_index, ["index.html"]},
 	 {mime_types, [
 				   {"html", "text/html"},
